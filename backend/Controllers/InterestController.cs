aa9afc94da053fe905d98b6d8277e0c9a112832f
Carl Åfalk <carl.afalk@hotmail.se>
2023-05-11 22:18:46 +0200

    added InterestController with 2 endpoints

---
+++ backend/Controllers/InterestController.cs
@@ -0,0 +1,32 @@
+using DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InterestController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    public InterestController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ActionResult<List<Interest>>> GetMyInterests(Guid userId)
+    {
+        var myInterests = new List<Interest>();
+        var myUserInterests = await _context.UserInterests.Where(x => x.UserId == userId).ToListAsync();
+        foreach(var item in myUserInterests)
+        {
+            myInterests.Add(await _context.Interests.Where(x => x.Id == item.InterestId).FirstOrDefaultAsync());
+        }
+
+        return Ok(myInterests);
+    }
+
+    public async Task<ActionResult<List<Interest>>> GetAllInterests(){
+        return await _context.Interests.ToListAsync();
+    }
+}
