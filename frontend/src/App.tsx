49c7200c419fdfa8d33f404979474283d2119f45
osci-the-orca <88oscste@skola.boras.se>
2023-05-12 02:59:19 +0200

    auth fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Layout from "./Layout";
@@ -10,11 +11,30 @@ import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 
 function App() {
-  const currentUser = useUser();
+  const { currentUser, token, setCurrentUser } = useUser();
+
+  const getAuth = async (token: string) => {
+    if (token) {
+      const response = await fetch("http://localhost:5296/authenticate/isAuthenticated", {
+        method: "GET",
+        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+      });
+      if (response.status === 200) {
+        const deserializedResponse = await response.json();
+        setCurrentUser(deserializedResponse);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (token != "") {
+      getAuth(token);
+    }
+  }, [token]);
 
   return (
     <Routes>
-      {currentUser.currentUser ? (
+      {currentUser?.token !== "" ? (
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Activities />} />
           <Route path="activities/:id" element={<Activity />} />
--- frontend/src/contexts/UserContext.tsx
+++ frontend/src/contexts/UserContext.tsx
@@ -1,19 +1,24 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
 interface User {
-    id: number;
-    username: string;
-    password: string;
-    gender: string;
-    token: string;
+  id: number;
+  username: string;
+  password: string;
+  gender: string;
+  token: string;
 }
-export interface SignInDto extends Pick<User, "username" | "password"> {username: string; password: string;}
+export interface SignInDto extends Pick<User, "username" | "password"> {
+  username: string;
+  password: string;
+}
+
 interface UserContext {
   currentUser: User | undefined;
   loginUser: (user: User) => void;
   token: string;
   setToken: (token: string) => void;
+  setCurrentUser: React.Dispatch<React.SetStateAction<User>>;
 }
 
 const UserContext = createContext<UserContext>({
@@ -21,18 +26,18 @@ const UserContext = createContext<UserContext>({
   loginUser: () => console.warn("No provider found."),
   token: "",
   setToken: () => console.warn("No provider found."),
+  setCurrentUser: () => console.log("no provider found"),
 });
 
 interface Props {
   children: ReactNode;
 }
 
-
 const UserProvider = ({ children }: Props) => {
   // const [currentUser, setCurrentUser] = useLocalStorage<User | undefined>("user", undefined);
-  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
-  const [token, setToken] = useLocalStorage<string>('token', "");
-  
+  const [currentUser, setCurrentUser] = useState<User>({ username: "", token: "", password: "password" } as User);
+  const [token, setToken] = useLocalStorage<string>("token", "");
+
   const loginUser = (user: User) => {
     if (user) {
       setCurrentUser(user);
@@ -47,11 +52,7 @@ const UserProvider = ({ children }: Props) => {
   //   }
   // }, [token]);
 
-  return (
-    <UserContext.Provider value={{ currentUser, loginUser, token, setToken }}>
-      {children}
-    </UserContext.Provider>
-  );
+  return <UserContext.Provider value={{ currentUser, loginUser, token, setToken, setCurrentUser }}>{children}</UserContext.Provider>;
 };
 
 export const useUser = () => useContext(UserContext);
