52c832dce652b97a8ed7c7522802ddb0746df2c7
Carl Åfalk <carl.afalk@hotmail.se>
2023-05-12 10:18:11 +0200

    added prop to inteterest and reseeded

--- backend/DAL/Entitys/Interest.cs
+++ backend/DAL/Entitys/Interest.cs
@@ -3,6 +3,7 @@ namespace DAL.Entities;
 public class Interest : BaseEntity
 {
     public string Name { get; set; } = String.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
 
     public virtual ICollection<UserInterest> UserInterests { get; set; }
 }
